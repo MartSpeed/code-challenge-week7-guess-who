8b8d5ec043fbb6ed40ac800dedaefa32b614ee3e
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-02 20:47:21 -0600

    I get the arrays now, still need to pick up the speed

--- client.js
+++ client.js
@@ -1,4 +1,13 @@
 //console.log('Here are all the available people:', people);
 
 // we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
+// change people array to gitName variable
 let gitName = people;
+
+// for (user of gitName) {
+//   console.log('this is the user', user.githubUsername);
+// }
+
+for (let i = 0; i < gitName.length; i++) {
+  console.log('I am in the for loop', gitName[i].githubUsername);
+}
