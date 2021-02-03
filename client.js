bcd7ce227c5ed044c2becce44535ff8eb07ff126
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-02 20:17:57 -0600

    renamed the people array and started checklist

--- client.js
+++ client.js
@@ -1,1 +1,4 @@
-console.log('Here are all the available people:', people);
+//console.log('Here are all the available people:', people);
+
+// we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
+let gitName = people;
