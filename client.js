f058c7a83ebd46057bbd9d5782a0d5eb4bcff2a8
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-02 20:53:13 -0600

    added the jquery library

--- client.js
+++ client.js
@@ -1,9 +1,17 @@
-//console.log('Here are all the available people:', people);
+console.log('Here are all the available people:', people);
 
-// we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
+/**
+ * we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
+ * 1. create a variable name for the array
+ * 2. create a loop that goes through the array of people
+ * 3. grab the github Username from the array
+ * 4. match the value for
+ */
+
 // change people array to gitName variable
 let gitName = people;
 
+// why does this return an error???
 // for (user of gitName) {
 //   console.log('this is the user', user.githubUsername);
 // }
