4efcbb0fd32a1b417899cbb9f4d9e808853b6df5
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-02 21:44:09 -0600

    have the array of employees with the correct name description

--- client.js
+++ client.js
@@ -1,21 +1,32 @@
 console.log('Here are all the available people:', people);
 
-/**
- * we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
- * 1. create a variable name for the array
- * 2. create a loop that goes through the array of people
- * 3. grab the github Username from the array
- * 4. match the value for
- */
+//start jQuery
+$(document).ready(onReady);
 
-// change people array to gitName variable
-let gitName = people;
+function onReady() {
+  console.log('jQuery Jonah Jameson');
+  /**
+   * we need a loop to cycle through the value of githubusername and use a template literal to insert that into a single line of code
+   * 1. create a variable name for the array
+   * 2. create a loop that goes through the array of people
+   * 3. grab the github Username from the array
+   * 4. match the value for
+   */
 
-// why does this return an error???
-// for (user of gitName) {
-//   console.log('this is the user', user.githubUsername);
-// }
+  // using a for of loop to go through the list of objects
+  // then grab the name {gitUser.name} and their githubUsername {gitUser.githubUsername}
+  // append the values to the dom using Jquery .append() method
+  for (gitUser of people) {
+    // using the $(this) method to append to the body
+    // using the link from the README.md I will append the correct values into the link to print out the full list on employees
+    $('body').append(`
+    <div>
+      <img src="https://github.com/${gitUser.githubUsername}.png?size=250" alt="Profile image of ${gitUser.name}">
+    </div>
+  `);
+  }
 
-for (let i = 0; i < gitName.length; i++) {
-  console.log('I am in the for loop', gitName[i].githubUsername);
+  // for (let i = 0; i < gitName.length; i++) {
+  //   console.log('I am in the for loop', gitName[i].githubUsername);
+  // }
 }
